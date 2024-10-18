ac316f55bc8749ac7cd594bfda80e608aaa25be8
huhao7 <947416983@qq.com>
2024-10-18 11:21:08 +0800

    添加组件1和组件2

---
+++ components/Component1.vue
@@ -0,0 +1,15 @@
+<!-- components/MyComponent.vue -->
+<template>
+  <div>
+    <p>组件1</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+p {
+  color: blue;
+  font-size: 20px;
+}
+</style>
